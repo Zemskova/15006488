749b0c930561eb48a6c449df30e499fa63ee4d77
mariia <isha@d219-betatron.paris-sorbonne.fr>
2015-10-15 10:53:20 +0200

    Importation du projet Java

---
+++ Mo,PremierProgramme/src/tp01/SimpleProgramme1.java
@@ -0,0 +1,24 @@
+package tp01;
+/** @author montacie*/ 
+/** @version 1.0 */
+import .Console
+/** quelles sont les fonctionnalités de la classe? */ 
+
+public class MaPremierConsole {
+	/** @param 
+	 * 
+	 * @param args parametre en ligne
+	 */
+	
+	/**
+	 * @param args parametre en ligne
+	 */
+	public static void main(String[] args) {
+// commentaire
+		Console c = new Console();
+		c.println("Claude");
+		c.println("Montacié");
+		
+	}
+
+}
